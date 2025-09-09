4b50fc1dd4e2609168be6d54c99b4a736df86de5
n <iyizireanique16@gmail.com>
2025-09-09 09:23:36 +0200

    under maintenamce

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -32,7 +32,7 @@
 const Index = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white flex-col">
-      <h1 className="text-4xl font-bold mb-4">.</h1>
+      <h1 className="text-4xl font-bold mb-4">Under Maintaince </h1>
       <p className="text-lg text-gray-300">
       </p>
     </div>
